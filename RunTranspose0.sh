23f6fdce7a466e72e2a3640d0823f3993e552f4a
SHIROKAMIQQ <migzguiang@gmail.com>
2025-05-18 11:22:16 +0800

    feat(transpose): add scripts to run perf command

---
+++ RunTranspose0.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for ((i=1; i<=100; i++))
+do
+    printf -v num "%03d" $i
+    perf stat -B -e all_data_cache_accesses,l2_cache_accesses_from_dc_misses,l2_cache_hits_from_dc_misses,l2_cache_misses_from_dc_misses,cycles,instructions ./transpose 16k.ppm 0 2> transpose-0-results/transpose-0.${num}.txt
+    echo "Run $i completed"
+done
+echo "All 100 runs completed"
